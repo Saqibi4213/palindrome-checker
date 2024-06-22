459139f456e90c60dcc65f401f704588622fb259
Saqibi4213 <mehriamosawer@gmail.com>
2024-06-22 20:01:44 +0430

    space-infix-ops

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
-const checkForPalindrome =input => {
-  const originalInput = input;
+const checkForPalindrome =(input) => {
+  const originalInput =(input);
 
   if (input === '') {
     alert('Please input a value');
