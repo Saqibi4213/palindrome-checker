ec1ff4c518c81981b5baa0337dcb747e0e1a4f39
Saqibi4213 <mehriamosawer@gmail.com>
2024-06-22 20:14:10 +0430

    spaces error

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
-const checkForPalindrome =(input) => {
-  const originalInput =(input);
+const checkForPalindrome = (input) => {
+  const originalInput = (input);
 
   if (input === '') {
-    alert('Please input a value');
     return;
   }
 
