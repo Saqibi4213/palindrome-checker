9da7a44520085528e8d584f6e9a9c7440c354b81
Saqibi4213 <mehriamosawer@gmail.com>
2024-06-27 00:32:24 +0430

    alert error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 const checkForPalindrome = (input) => {
-  if (!input === '') {
+  if (input === '') {
     alert('Please input a value');
     return;
   }
